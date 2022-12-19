b252eafcd657119a236143ee162bf3c3d7d2032e
scooomaker <scooomaker@foxmail.com>
2022-12-19 17:50:41 +0800

    modify

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ numbers = list(range(1, 27))
 window = pyglet.window.Window()
 pyglet.gl.glClearColor(1,1,1,1)
 
+
 label = pyglet.text.Label(text='', font_size=90, x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center',color=(0, 0, 0, 255))
 
 @window.event
