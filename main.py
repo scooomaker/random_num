a8815d612bb7c0ea91c93b8b809c839fe8a77530
scooomaker <scooomaker@foxmail.com>
2022-12-20 20:31:57 +0800

    modify

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 class MainFrame(wx.Frame):
     def __init__(self):
         super().__init__(None, title='随机数生成器(1-26) for class 17')
-        self.SetSize((560, 300))
+        self.SetSize((700, 300))
         self.Center()
 
         # create menu bar
@@ -29,10 +29,10 @@ class MainFrame(wx.Frame):
         self.mode_1_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.mode_1_panel.SetSizer(self.mode_1_sizer)
 
-        font = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        fontButton = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         
         self.mode_1_button = wx.Button(self.mode_1_panel, label='生成',size=(150, 50))
-        self.mode_1_button.SetFont(font) # 设置按钮字体大小
+        self.mode_1_button.SetFont(fontButton) # 设置按钮字体大小
         self.mode_1_sizer.Add(self.mode_1_button, 0, wx.ALL, 85)
         self.mode_1_text = wx.StaticText(self.mode_1_panel, label='', style=wx.ALIGN_BOTTOM)
 
@@ -47,40 +47,56 @@ class MainFrame(wx.Frame):
         self.mode_2_panel = wx.Panel(self.panel)
         self.mode_2_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.mode_2_panel.SetSizer(self.mode_2_sizer)
+
+
         self.mode_2_button = wx.Button(self.mode_2_panel, label='生成')
-        self.mode_2_sizer.Add(self.mode_2_button, 0, wx.ALL, 5)
-        self.mode_2_text = wx.StaticText(self.mode_2_panel, label='', style=wx.ALIGN_CENTER)
+        self.mode_2_button.SetFont(fontButton) # 设置按钮字体大小
+        self.mode_2_sizer.Add(self.mode_2_button, 0, wx.ALL, 85)
+        self.mode_2_text = wx.StaticText(self.mode_2_panel, label='', style=wx.ALIGN_BOTTOM)
 
 
         font = wx.Font(30, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
         self.mode_2_text.SetFont(font)
 
-        self.mode_2_sizer.Add(self.mode_2_text, 0, wx.ALL, 5)
+        self.mode_2_sizer.Add(self.mode_2_text, 0, wx.ALL, 85)
         self.Bind(wx.EVT_BUTTON, self.on_mode_2_button, self.mode_2_button)
         self.sizer.Add(self.mode_2_panel, 0, wx.EXPAND)
         self.mode_2_panel.Hide()
 
 
          # create mode 3 panel
+
+
+        font = wx.Font(16, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+
         self.mode_3_panel = wx.Panel(self.panel)
+
         self.mode_3_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.mode_3_panel.SetSizer(self.mode_3_sizer)
         self.mode_3_text_1 = wx.StaticText(self.mode_3_panel, label='输入整数，从')
+        self.mode_3_text_1.SetFont(font)
         self.mode_3_sizer.Add(self.mode_3_text_1, 0, wx.ALL, 5)
+
         self.mode_3_text_ctrl_1 = wx.TextCtrl(self.mode_3_panel)
         self.mode_3_sizer.Add(self.mode_3_text_ctrl_1, 0, wx.ALL, 5)
+
         self.mode_3_text_2 = wx.StaticText(self.mode_3_panel, label='到')
+        self.mode_3_text_2.SetFont(font)
         self.mode_3_sizer.Add(self.mode_3_text_2, 0, wx.ALL, 5)
+
         self.mode_3_text_ctrl_2 = wx.TextCtrl(self.mode_3_panel)
         self.mode_3_sizer.Add(self.mode_3_text_ctrl_2, 0, wx.ALL, 5)
+
         self.mode_3_button = wx.Button(self.mode_3_panel, label='生成')
+        self.mode_3_button.SetFont(font)
+
         self.mode_3_sizer.Add(self.mode_3_button, 0, wx.ALL, 5)
         self.mode_3_text = wx.StaticText(self.mode_3_panel, label='', style=wx.ALIGN_CENTER)
 
         font = wx.Font(30, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
         self.mode_3_text.SetFont(font)
 
-        self.mode_3_sizer.Add(self.mode_3_text, 0, wx.ALL, 5)
+        self.mode_3_sizer.Add(self.mode_3_text, 0, wx.ALL, 8)
         self.Bind(wx.EVT_BUTTON, self.on_mode_3_button, self.mode_3_button)
         self.sizer.Add(self.mode_3_panel, 0, wx.EXPAND)
         self.mode_3_panel.Hide()
